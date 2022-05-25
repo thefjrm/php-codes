e9828c07964414ae6fddda1223cf5314fdd20434
thefjrm <felipemartinsplayernet@gmail.com>
2022-05-25 19:36:46 -0300

    Create tipos.php

---
+++ tipos.php
@@ -0,0 +1,13 @@
+<?php
+$int = 18;
+$float = 18.5;
+$string = "João";
+echo gettype($int); /* Verificando tipo da variável */
+echo "\n";
+echo gettype($float);
+echo "\n"; /* \n = quebra de linha ou PHP_EOL */
+echo gettype($string);
+echo "\n";
+echo "Olá, $string \n";
+echo "Você tem $int anos";
+echo "Eu tenho medo." . PHP_EOL;
